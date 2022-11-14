8db035aad10c44f545f6c5a613a210789d1409ce
yuchen19951220 <156363623@qq.com>
2022-11-14 22:17:42 +0800

    first commmit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,164 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+//计数器
+var interval = null;
+
+//值越大旋转时间越长  即旋转速度
+var intime = 50;
+
+Page({
+  data: {
+    color: [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+    //9张奖品图片
+    images: ['/images/drawable_s21.png', '/images/drawable_s22.png', '/images/drawable_s23.png', '/images/drawable_s24.png', '/images/drawable_s25.png', '/images/drawable_s26.png', '/images/drawable_s27.png', '/images/drawable_s28.png', '/images/drawable_s29.png'],
+    btnconfirm: '/images/start_game.png',
+    clickLuck:'clickLuck',
+    luckPosition:0,
+  },
+
+  onLoad:function(){
+    this.loadAnimation();
+  },
+
+
+  //点击抽奖按钮
+  clickLuck:function(){
+    this.luckPosition = Math.floor(Math.random()*8);
+    var e = this;
+
+    //判断中奖位置格式
+    if (e.data.luckPosition == null || isNaN(e.data.luckPosition) || e.data.luckPosition>7){
+      return;
+    }
+
+
+    //设置按钮不可点击
+    e.setData({
+      btnconfirm:'/images/start_game_press.png',
+      clickLuck:'',
+    })
+    //清空计时器
+    clearInterval(interval);
+    var index = 0;
+    console.log(e.data.color[0]);
+    //循环设置每一项的透明度
+    interval = setInterval(function () {
+      if (index > 7) {
+        index = 0;
+        e.data.color[7] = 0.5
+      } else if (index != 0) {
+        e.data.color[index - 1] = 0.5
+      }
+      e.data.color[index] = 1
+      e.setData({
+        color: e.data.color,
+      })
+      index++;
+    }, intime);
+
+    //模拟网络请求时间  设为两秒
+    var stoptime = 300;
+    setTimeout(function () {
+      e.stop(e.data.luckPosition);
+    }, stoptime)
+
+  },
+
+  //也可以写成点击按钮停止抽奖
+  // clickStop:function(){
+  //   var stoptime = 2000;
+  //   setTimeout(function () {
+  //     e.stop(1);
+  //   }, stoptime)
+  // },
+
+  stop: function (which){
+    var e = this;
+    //清空计数器
+    clearInterval(interval);
+    //初始化当前位置
+    var current = -1;
+    var color = e.data.color;
+    for (var i = 0; i < color.length; i++) {
+      if (color[i] == 1) {
+        current = i;
+      }
+    }
+    //下标从1开始
+    var index = current + 1;
+
+    e.stopLuck(which, index, intime, 10);
+  },
+
+
+/**
+ * which:中奖位置
+ * index:当前位置
+ * time：时间标记
+ * splittime：每次增加的时间 值越大减速越快
+ */
+  stopLuck: function (which, index,time,splittime){
+    var e = this;
+    //值越大出现中奖结果后减速时间越长
+    var color = e.data.color;
+    setTimeout(function () {
+      //重置前一个位置
+      if (index > 7) {
+        index = 0;
+        color[7] = 0.5
+      } else if (index != 0) {
+        color[index - 1] = 0.5
+      }
+      //当前位置为选中状态
+      color[index] = 1
+      e.setData({
+        color: color,
+      })
+          //如果旋转时间过短或者当前位置不等于中奖位置则递归执行
+          //直到旋转至中奖位置
+        if (time < 200 || index != which){
+          //越来越慢
+          splittime++;
+          time += splittime;
+          //当前位置+1
+          index++;
+          e.stopLuck(which, index, time, splittime);
+        }else{
+        //1秒后显示弹窗
+          setTimeout(function () {
+            e.loadAnimation();
+            wx.navigateToMiniProgram({
+            appId: 'wx3a8b84b5f1326c37',
+            })
+            e.setData({
+            btnconfirm: '/images/start_game.png',
+            clickLuck: 'clickLuck',
+            })
+          }, 500);
+        }
+    }, time);
+    console.log(time);
+  },
+  //进入页面时缓慢切换
+ loadAnimation:function (){
+  var e = this;
+  var index = 0;
+  // if (interval == null){
+  interval = setInterval(function () {
+    if (index > 7) {
+      index = 0;
+      e.data.color[7] = 0.5
+    } else if (index != 0) {
+      e.data.color[index - 1] = 0.5
+    }
+    e.data.color[index] = 1
+    e.setData({
+      color: e.data.color,
+    })
+    index++;
+  }, 1000);
+  // }  
+}
+})
